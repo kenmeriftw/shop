e5d9640e142bf76511252d13a7797d4f7c754abe
knmrftw <2669997@gmail.com>
2021-02-28 12:29:53 +0300

    27-1

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,15 @@
 class Movie < Product
+  attr_reader :director, :year, :title
+
+  def initialize(params)
+    super
+
+    @title = params[:title]
+    @director = params[:director]
+    @year = params[:year]
+  end
+
+  def to_s
+    "Фильм #{@title}, #{@year}, режиссер #{@director}, #{super}"
+  end
 end
--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/movie'
 
-leon = Movie.new(price: 290, amount: 1)
+products = []
 
-puts "Фильм Леони стоит #{leon.price} рублей"
+products << Movie.new(title: 'Леон', director: 'Люк Бессон', year: 1994, price: 990, amount: 5)
+
+products << Movie.new(title: 'Дурак', director: 'Юрий Быков', year: 2014, price: 550, amount: 2)
+
+products << Book.new(title: 'Идиот', author: 'Федор Достоевский', genre: 'Роман', price: 1500, amount: 10)
+
+products.each { |product| puts product }
